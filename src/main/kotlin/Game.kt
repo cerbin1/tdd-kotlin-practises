f7347c479c7e0b7bf88e5c4567ffc663f3cd73aa
cerbin <gekarez@gmail.com>
2023-10-14 15:59:07 +0200

    Refactor

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,42 +1,41 @@
 class Game {
     var points: Int = 0
     var previousTurnPoints: Int = 0
-    var turnPoints: Int = 0
+//    var turnPoints: Int = 0
     var rollNumber: Int = 1
-    var bonus: Int = 0
+    var bonusPointsMultiplierCount: Int = 0
 
     fun roll(rollPoints: Int) {
         if (rollPoints < 0) {
             throw IllegalArgumentException()
         }
         points += rollPoints
-        if (bonus > 0) {
+        if (bonusPointsMultiplierCount > 0) {
             points += rollPoints
-            bonus--
+            bonusPointsMultiplierCount--
         }
-        turnPoints = rollPoints
 
-        if (isFirstRoll() && points == 10) {
-            bonus += 2
-            turnPoints = 0
+        if (isStrike(rollPoints)) {
+            bonusPointsMultiplierCount += 2
             previousTurnPoints = 0
         }
 
         if (isSecondRoll()) {
-            if (isSpare()) {
-                bonus++
+            if (isSpare(rollPoints)) {
+                bonusPointsMultiplierCount++
             }
-            turnPoints = 0
             previousTurnPoints = 0
         }
 
-        previousTurnPoints = turnPoints
+        previousTurnPoints = rollPoints
         rollNumber++
     }
 
-    private fun isFirstRoll() = rollNumber % 2 == 1
+    private fun isSpare(rollPoints: Int) = rollPoints + previousTurnPoints == 10
 
-    private fun isSpare() = turnPoints + previousTurnPoints == 10
+    private fun isStrike(rollPoints: Int) = isFirstRoll() && rollPoints == 10
+
+    private fun isFirstRoll() = rollNumber % 2 == 1
 
     private fun isSecondRoll() = !isFirstRoll()
 }
